047ef4ff52c8afca3dede911724e968519bfbc10
Haleem <104878081+Haleem-code@users.noreply.github.com>
2025-10-31 08:30:24 +0100

    Change PLATFORM_WALLET address
    Updated the PLATFORM_WALLET address to the new deployment address.

--- programs/betting-contract/src/constants.rs
+++ programs/betting-contract/src/constants.rs
@@ -3,7 +3,5 @@ use anchor_lang::prelude::*;
 #[constant]
 pub const SEED: &str = "anchor";
 
-// Platform wallet address - Replace with your actual platform wallet address when deploying
-// For now, using the program ID as placeholder
-pub static PLATFORM_WALLET: Pubkey = pubkey!("DRNEUsSx9gNre6f6mLFhrHDVRDfD4eMGu68dussziUgi");
+pub static PLATFORM_WALLET: Pubkey = pubkey!("HHEvS6uKozLLA4HkV8pujd7AKBDX9rbjWLWerxGc8swc");
 
